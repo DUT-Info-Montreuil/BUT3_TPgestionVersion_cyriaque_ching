4fbc7c644957b87a179662439c64031fae757525
Emilio Cyriaque <ems@gmail.com>
2023-09-22 15:26:34 +0200

    ajout des nouvelles images rambo pour remplacer l'ancien visuelle du player

---
+++ src/main/java/fr/iut/montreuil/S05_R08/tp01_GestionVersion/ressources/HUD/menu/Snippet.java
@@ -0,0 +1,20 @@
+package fr.iut.montreuil.S05_R08.tp01_GestionVersion.ressources.HUD.menu;
+
+public class Snippet {
+	Numéro de version du produit (pom.xml) : 2 .0.0
+	Version sur les environnements :
+	Dev :
+	
+	
+	Dev Ref :
+	
+	
+	Recette :
+	
+	
+	Production :
+	
+	
+	
+}
+
